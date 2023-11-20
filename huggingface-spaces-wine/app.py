e7777c1d70ecb27613c2deab2713d3b300543f33
dengty1998 <dengtianyu98@gmail.com>
2023-11-20 22:15:10 +0200

    feat, implement basic quality prediction

--- huggingface-spaces-wine/app.py
+++ huggingface-spaces-wine/app.py
@@ -10,17 +10,17 @@ fs = project.get_feature_store()
 
 
 mr = project.get_model_registry()
-model = mr.get_model("wine_model", version=1)
+model = mr.get_model("wine_model", version=2)
 model_dir = model.download()
 model = joblib.load(model_dir + "/wine_model.pkl")
 print("Model downloaded")
 
 
-def wine(volatile_acidity, total_sulfur_dioxide):
+def wine(volatile_acidity, chlorides, density, alcohol):
     print("Calling function")
 #     df = pd.DataFrame([[sepal_length],[sepal_width],[petal_length],[petal_width]],
-    df = pd.DataFrame([[volatile_acidity, total_sulfur_dioxide]],
-                      columns=['volatile_acidity', 'total_sulfur_dioxide'])
+    df = pd.DataFrame([[volatile_acidity, chlorides, density, alcohol]],
+                      columns=['volatile_acidity', 'chlorides', 'density', 'alcohol'])
     print("Predicting")
     print(df)
     # 'res' is a list of predictions returned as the label.
@@ -29,9 +29,9 @@ def wine(volatile_acidity, total_sulfur_dioxide):
     # the first element.
 #     print("Res: {0}").format(res)
     print(res)
-    if res == "red":
+    if res == 3 or res == 4 or res == 5 or res == 6:
         wine_url = "https://upload.wikimedia.org/wikipedia/en/c/c0/Red_Wine_Glass.jpg"
-    elif res == "white":
+    elif res == 7 or res == 8 or res == 9:
         wine_url = "https://upload.wikimedia.org/wikipedia/commons/7/71/White_Wine_Glas.jpg"
     response = requests.get(wine_url, stream=True)
     print("Content-Type:", response.headers.get('Content-Type'))
@@ -46,7 +46,9 @@ demo = gr.Interface(
     allow_flagging="never",
     inputs=[
         gr.inputs.Number(default=0.5, label="volatile acidity"),
-        gr.inputs.Number(default=50.0, label="total sulfur dioxide"),
+        gr.inputs.Number(default=0.1, label="chlorides"),
+        gr.inputs.Number(default=1.000, label="density"),
+        gr.inputs.Number(default=10.0, label="alcohol"),
     ],
     outputs=gr.Image(type="pil"))
 
--- wine-batch-inference-pipeline.py
+++ wine-batch-inference-pipeline.py
@@ -28,23 +28,23 @@ def g():
     fs = project.get_feature_store()
 
     mr = project.get_model_registry()
-    model = mr.get_model("wine_model", version=1)
+    model = mr.get_model("wine_model", version=2)
     model_dir = model.download()
     model = joblib.load(model_dir + "/wine_model.pkl")
 
-    feature_view = fs.get_feature_view(name="wine", version=1)
+    feature_view = fs.get_feature_view(name="wine", version=4)
     batch_data = feature_view.get_batch_data()
 
     y_pred = model.predict(batch_data)
     # print(y_pred)
     # need to change the offset manually to have a confution matrix
-    offset = 3
+    offset = 1
     wine = y_pred[y_pred.size-offset]
-    if wine == "red":
+    if wine == 3 or wine == 4 or wine == 5 or wine == 6:
         wine_url = "https://upload.wikimedia.org/wikipedia/en/c/c0/Red_Wine_Glass.jpg"
-    elif wine == "white":
+    elif wine == 7 or wine == 8 or wine == 9:
         wine_url = "https://upload.wikimedia.org/wikipedia/commons/7/71/White_Wine_Glas.jpg"
-    print("Wine predicted: " + wine)
+    print("Wine quality predicted: " + "wine")
     response = requests.get(wine_url, stream=True)
     print("Content-Type:", response.headers.get('Content-Type'))
     img = Image.open(response.raw)
@@ -52,22 +52,22 @@ def g():
     dataset_api = project.get_dataset_api()
     dataset_api.upload("./latest_wine.jpg", "Resources/images", overwrite=True)
 
-    wine_fg = fs.get_feature_group(name="wine", version=1)
+    wine_fg = fs.get_feature_group(name="wine", version=4)
     df = wine_fg.read()
     # print(df)
-    label = df.iloc[-offset]["type"]
-    if label == "red":
+    label = df.iloc[-offset]["quality"]
+    if label == 3 or label == 4 or label == 5 or label == 6:
         label_url = "https://upload.wikimedia.org/wikipedia/en/c/c0/Red_Wine_Glass.jpg"
-    elif label == "white":
+    elif label == 7 or label == 8 or label == 9:
         label_url = "https://upload.wikimedia.org/wikipedia/commons/7/71/White_Wine_Glas.jpg"
-    print("Wine actual: " + label)
+    print("Wine quality actual: " + "label")
     response = requests.get(label_url, stream=True)
     img = Image.open(response.raw)
     img.save("./actual_wine.jpg")
     dataset_api.upload("./actual_wine.jpg", "Resources/images", overwrite=True)
 
     monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
-                                                version=1,
+                                                version=2,
                                                 primary_key=["datetime"],
                                                 description="Wine flower Prediction/Outcome Monitoring"
                                                 )
@@ -94,22 +94,25 @@ def g():
     labels = history_df[['label']]
 
     # Only create the confusion matrix when our iris_predictions feature group has examples of all 3 iris flowers
-    print("Number of different flower predictions to date: " +
+    print("Number of different wine predictions to date: " +
           str(predictions.value_counts().count()))
-    if predictions.value_counts().count() == 2:
-        results = confusion_matrix(labels, predictions)
+    # if predictions.value_counts().count() == 7:
+    results = confusion_matrix(
+        labels, predictions, labels=[3, 4, 5, 6, 7, 8, 9])
 
-        df_cm = pd.DataFrame(results, ['True Red', 'True White'],
-                             ['Pred Red', 'Pred White'])
+    df_cm = pd.DataFrame(results, ['True Q3', 'True Q4', 'True Q5', 'True Q6', 'True Q7', 'True Q8', 'True Q9'],
+                         ['Pred Q3', 'Pred Q4', 'Pred Q5', 'Pred Q6', 'Pred Q7', 'Pred Q8', 'Pred Q9'])
 
-        cm = sns.heatmap(df_cm, annot=True)
-        fig = cm.get_figure()
-        fig.savefig("./confusion_matrix.png")
-        dataset_api.upload("./confusion_matrix.png",
-                           "Resources/images", overwrite=True)
-    else:
-        print("You need 2 different wine predictions to create the confusion matrix.")
-        print("Run the batch inference pipeline more times until you get 2 different wine predictions")
+    cm = sns.heatmap(df_cm, annot=True)
+    fig = cm.get_figure()
+    fig.savefig("./confusion_matrix.png")
+    dataset_api.upload("./confusion_matrix.png",
+                       "Resources/images", overwrite=True)
+
+
+"""     else:
+        print("You need 7 different wine predictions to create the confusion matrix.")
+        print("Run the batch inference pipeline more times until you get 7 different wine predictions") """
 
 
 if __name__ == "__main__":
